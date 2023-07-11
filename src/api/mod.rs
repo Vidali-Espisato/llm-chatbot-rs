ed4ede53a6e63d781fe52722e9775d8042c9fec9
Manish Roy <manish.11612939@lpu.in>
2023-07-12 00:04:20 +0530

    added inference_callback to prevent llm model's self state updates

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use cfg_if::cfg_if;
 use crate::model::conversation::Conversation;
 
 
@@ -55,4 +56,43 @@ pub async fn converse(cx: Scope, conversation: Conversation) -> Result<(), Serve
     ).unwrap_or_else(|e| panic!("{e}"));
 
     Ok(())
+}
+
+
+cfg_if! {
+    if #[cfg(feature = "ssr")] {
+    use std::convert::Infallible;
+        fn inference_callback<'a>(
+            stop_sequence: String,
+            buf: &'a mut String,
+            out_str: &'a mut String,
+        ) -> impl FnMut(llm::InferenceResponse) -> Result<llm::InferenceFeedback, Infallible> + 'a {
+            use llm::InferenceFeedback::Halt;
+            use llm::InferenceFeedback::Continue;
+
+            move |resp| match resp {
+                llm::InferenceResponse::InferredToken(t) => {
+                    let mut reverse_buf = buf.clone();
+                    reverse_buf.push_str(t.as_str());
+                    if stop_sequence.as_str().eq(reverse_buf.as_str()) {
+                        buf.clear();
+                        return Ok::<llm::InferenceFeedback, Infallible>(Halt);
+                    } else if stop_sequence.as_str().starts_with(reverse_buf.as_str()) {
+                        buf.push_str(t.as_str());
+                        return Ok(Continue);
+                    }
+
+                    if buf.is_empty() {
+                        out_str.push_str(&t);
+                    } else {
+                        out_str.push_str(&reverse_buf);
+                    }
+
+                    Ok(Continue)
+                }
+                llm::InferenceResponse::EotToken => Ok(Halt),
+                _ => Ok(Continue),
+            }
+        }
+    }
 }
